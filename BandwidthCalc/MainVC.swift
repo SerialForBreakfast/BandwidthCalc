bcf49d1de3718fe8667e45a3179421a68dba8297
SerialForBreakfast <joe@showblender.com>
2019-04-17 11:16:13 -0700

    IBDesignable not updating in IB

--- BandwidthCalc/MainVC.swift
+++ BandwidthCalc/MainVC.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet var dataRateTextFieldOutlet: DataRateTextField!
+    
+    @IBOutlet var dataCapacityTextFieldOutlet: DataRateTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        dataRateTextFieldOutlet.inputAccessoryView = calcBtn
+        dataCapacityTextFieldOutlet.inputAccessoryView = calcBtn
+        
     }
-
+    
+    @objc func calculate() {
+        print("@obj calculate method working")
+    }
 
 }
 
--- BandwidthCalc/Views/DataRateTextField.swift
+++ BandwidthCalc/Views/DataRateTextField.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
+
+@IBDesignable
 class DataRateTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    func customizeView() {
         backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
         if placeholder == nil {
-            placeholder = " "
+            placeholder = "Undefined"
         }
         
-        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: UIColor.white])
+        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: UIColor.black])
         
         attributedPlaceholder = place
-        textColor = UIColor.white
+        textColor = UIColor.black
+        
     }
-    
 }
